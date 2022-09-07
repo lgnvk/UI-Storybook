fc0d3e7d37f5a6179578e8f8779e16f84fa07431
Konstantin Loginov <k.s.loginoff@gmail.com>
2022-09-07 14:33:27 +0400

    Мультиселект

--- src/components/Multiselect/Multiselect.tsx
+++ src/components/Multiselect/Multiselect.tsx
@@ -18,58 +18,76 @@ type OptionType = {
 
 const Multiselect: FC<MultiselectType> = ({ name, options, theme }) => {
   const [optionsmod, setOptions] = useState(options);
+  const [optionsInput, setInputOptions] = useState<any[]>([]);
   const [active, setActive] = useState(false);
-
   const cx = cn.bind(styles);
+
   const activeToggler = () => {
     setActive(!active);
   };
-  const checkToggler = (id: number, event: any) => {
+
+  const checkToggler = (event: any, option: OptionType) => {
     event.stopPropagation();
     const newOptions = [...optionsmod];
-    newOptions[id - 1].checked = !newOptions[id - 1].checked;
+    newOptions[option.id - 1].checked = !newOptions[option.id - 1].checked;
     setOptions(newOptions);
+
+    let inputOptions = optionsInput;
+    if (option.checked) {
+      inputOptions.push(option);
+    } else {
+      inputOptions = inputOptions.filter((option) => option.checked);
+    }
+    setInputOptions(inputOptions);
   };
+
   return (
     <div className={cx("multiselect", theme)}>
       <div className={cx("multiselect__name")}>{name}</div>
       <div className="multiselect__wrapper">
         <button
           className={cx("multiselect__button")}
-          onClick={() => activeToggler()}
-        >
-          {optionsmod.map((el) => (
-            <div>
-              {el.checked && (
-                <div
-                  className={cx("multiselect__label")}
-                  onClick={(event) => checkToggler(el.id, event)}
-                >
-                  <Label theme={theme} withCross="true">
-                    {el.name}
-                  </Label>
-                </div>
-              )}
+          onClick={() => activeToggler()}>
+          {optionsInput[0] && (
+            <div
+              className={cx("multiselect__label")}
+              onClick={(event) => checkToggler(event, optionsInput[0])}>
+              <Label theme={theme} withCross="true">
+                {optionsInput[0].name}
+              </Label>
             </div>
-          ))}
+          )}
+          {optionsInput[1] && (
+            <div
+              className={cx("multiselect__label")}
+              onClick={(event) => checkToggler(event, optionsInput[1])}>
+              <Label theme={theme} withCross="true">
+                {optionsInput[1].name}
+              </Label>
+            </div>
+          )}
+          {optionsInput.length > 2 && (
+            <div className={cx("multiselect__dots")}>...</div>
+          )}
         </button>
+
         <div
           className={cx("multiselect__options", {
             multiselect__options_active: active,
-          })}
-        >
-          {options.map((el) => (
+          })}>
+          {options.map((option) => (
             <div
-              key={el.id}
+              key={option.id}
               className={cx("multiselect__option")}
-              onClick={(event) => checkToggler(el.id, event)}
-            >
+              onClick={(event) => checkToggler(event, option)}>
               <Checkbox
                 theme={theme}
-                isChecked={el.checked}
+                isChecked={option.checked}
                 className={cx("multiselect__option-checkbox")}
               />
-              <div className={cx("multiselect__option-text")}>{el.name}</div>
+              <div className={cx("multiselect__option-text")}>
+                {option.name}
+              </div>
             </div>
           ))}
         </div>
