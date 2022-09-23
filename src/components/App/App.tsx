a9898c1ec5ea026a7053be6dc9b596e69a512375
Konstantin Loginov <k.s.loginoff@gmail.com>
2022-09-23 11:43:30 +0400

    s

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Что-то будет здесь</div>;
+  return <div className="App">Что-то будет здесь. И ещё</div>;
 }
 
 export default App;
